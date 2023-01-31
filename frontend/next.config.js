127445f4df85591beb4f9abc778b8f7ec35bd95d
Prosper Success <osiletusuccess@gmail.com>
2023-01-31 15:19:57 +0100

    netlify build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,26 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+	domains: ['cdn.sanity.io'],
+	loader: 'custom'
+}
 }
 
 
 module.exports = nextConfig
 
-
-
-module.exports = {
-	images: {
-		domains: ['cdn.sanity.io'],
-		loader: 'custom'
-	}
-};
-// module.exports = {
-
-// }
-
-// module.exports = {
-// 	images: {
-// 		domains: ['cdn.sanity.io'],
-// 		loader: 'custom'
-// 	}
-// };
