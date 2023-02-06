536b6beb37efa568ed0e587b278e2ea8637e899a
Prosper Success <osiletusuccess@gmail.com>
2023-02-06 01:01:57 +0100

    cors access

--- frontend/lib/fetcher.js
+++ frontend/lib/fetcher.js
@@ -2,13 +2,19 @@ import UseSWR from 'swr';
 
 const baseURL = "http://localhost:3000/";
 
-const response = (...args) => fetch(...args).then(res => res.json())
+const response = (...args) =>
+  fetch(...args, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  })
+    .then(res => res.json());
 
-export default function fetcher(endpoint){
-    const { data, error } = UseSWR(`${baseURL}${endpoint}`, response)
-    return {
-        data,
-        isLoading : !error && !data,
-        isError : error
-    }
+export default function fetcher(endpoint) {
+  const { data, error } = UseSWR(`${baseURL}${endpoint}`, response);
+  return {
+    data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 }
--- frontend/lib/helper.js
+++ frontend/lib/helper.js
@@ -1,10 +1,10 @@
-
-
 const baseURL = "http://localhost:3000/api/posts";
 
-// endpoint: http://localhost:3000/api/posts
 export default async function getPost(id){
-    const res = await fetch(`{ mode: 'no-cors' }, ${baseURL}`)
+    const headers = new Headers();
+    headers.append("Access-Control-Allow-Origin", "*");
+
+    const res = await fetch(`${baseURL}`, { headers });
     const posts = await res.json()
 
     if(id){
